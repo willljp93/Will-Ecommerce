327966ce092d1829f01e35b4669f19f663179fff
willjp93 <willjp93@gmail.com>
2023-03-13 17:29:48 -0400

    cambios a la tabla

--- src/pages/Admin/TablesDataPage.vue
+++ src/pages/Admin/TablesDataPage.vue
@@ -49,8 +49,7 @@
             label="Agregar"
             @click="showAddDialog = true"
             class="q-ml-md"
-          >
-          </q-btn>
+          />
           <q-btn
             flat
             round
@@ -68,7 +67,7 @@
               dense
               color="primary"
               icon="edit"
-              @click="editFeatured(props.row)"
+              @click="showEditDialog = true"
             />
             <q-btn
               flat
@@ -76,7 +75,7 @@
               dense
               color="negative"
               icon="delete"
-              @click="deleteFeatured(props.row)"
+              @click="deleteFeatured(props.row.id)"
             />
           </q-td>
         </template>
@@ -84,7 +83,7 @@
       <q-dialog v-model="showAddDialog" persistent>
         <q-card>
           <q-card-section>
-            <q-form @submit="addFeatured">
+            <q-form @submit="addFeatured(props.row)">
               <q-input v-model="newFeatured.name" label="Nombre" />
               <q-input v-model="newFeatured.description" label="Descripción" />
               <q-input
@@ -113,7 +112,42 @@
           </q-card-section>
         </q-card>
       </q-dialog>
-      <!-- -------------------- -->
+
+      <q-dialog v-model="showEditDialog" persistent>
+        <q-card>
+          <q-card-section>
+            <q-form @submit="editFeatured(props.row)">
+              <q-input v-model="editedFeatured.name" label="Nombre" />
+              <q-input
+                v-model="editedFeatured.description"
+                label="Descripción"
+              />
+              <q-input
+                v-model="editedFeatured.price"
+                label="Precio"
+                type="number"
+              />
+              <q-select
+                v-model="editedFeatured.rating"
+                label="Valoración"
+                :options="[1, 2, 3, 4, 5]"
+              />
+              <q-checkbox v-model="editedFeatured.discount" label="Descuento" />
+              <q-input
+                v-if="editedFeatured.discount"
+                v-model="editedFeatured.discountValue"
+                type="number"
+                label="Valor del descuento"
+              />
+              <q-input v-model="editedFeatured.image" label="Imagen" />
+              <div class="q-mt-md q-gutter-xs">
+                <q-btn type="submit" label="Agregar" color="primary" />
+                <q-btn label="Cancelar" @click="showEditDialog = false" />
+              </div>
+            </q-form>
+          </q-card-section>
+        </q-card>
+      </q-dialog>
     </div>
   </q-page>
 </template>
@@ -124,6 +158,7 @@ const featureds = ref([]);
 const filter = ref("");
 
 const showAddDialog = ref(false);
+const showEditDialog = ref(false);
 
 onMounted(async () => {
   await getFeatureds();
@@ -179,11 +214,7 @@ const columnsfeatureds = [
     align: "center",
   },
   { name: "image", label: "Imagen", field: "image", align: "center" },
-  {
-    name: "actions",
-    label: "Acciones",
-    align: "right",
-  },
+  { name: "actions", label: "Acciones", align: "right" },
 ];
 const newFeatured = ref({
   name: "",
@@ -194,17 +225,40 @@ const newFeatured = ref({
   image: "",
 });
 
-const addFeatured = async () => {
+const editedFeatured = ref({
+  name: "",
+  description: "",
+  price: 0,
+  rating: 1,
+  discountValue: "",
+  image: "",
+});
+
+const addFeatured = async (featured) => {
   try {
-    // Agregar la nueva fila a la lista de featureds
-    const { data } = await api.post("/collections/featureds/records", {
-      fields: newFeatured.value,
+    await api.post("collections/featureds/records", featured);
+    // Agregar la nueva fila a la tabla utilizando el resultado de la llamada a la API
+    // ...
+    showAddDialog.value = false;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const editFeatured = async (row) => {
+  try {
+    // Actualizar el elemento en el API
+    await api.patch(`/collections/featureds/records/${props.row}`, {
+      fields: editedFeatured.value,
     });
-    featureds.value.push(data);
+
+    // Actualizar el elemento en la lista de featureds
+    const index = featureds.value.findIndex((f) => f.id === props.row.id);
+    featureds.value[index] = editedFeatured.value;
 
     // Cerrar el diálogo y limpiar los campos del formulario
-    showAddDialog.value = false;
-    newFeatured.value = {
+    showEditDialog.value = false;
+    editedFeatured.value = {
       name: "",
       description: "",
       price: 0,
@@ -217,18 +271,13 @@ const addFeatured = async () => {
     console.error(error.response.data);
     // Mostrar mensaje de error al usuario
     alert(
-      "Ocurrió un error al agregar el elemento. Por favor, inténtalo de nuevo más tarde."
+      "Ocurrió un error al editar el elemento. Por favor, inténtalo de nuevo más tarde."
     );
   }
 };
 
-const editFeatured = (props) => {
-  // Agregar lógica para editar una fila
-  console.log(`Editado: ${props}`);
-};
-const deleteFeatured = (props) => {
-  // Agregar lógica para eliminar una fila
-  console.log(`Eliminado: ${props}`);
+const deleteFeatured = async (id) => {
+  await api.delete(`collections/featureds/records/${id}`);
 };
 </script>
 <style lang="scss">
